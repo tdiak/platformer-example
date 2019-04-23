2fb9ff57aacd10ba230d7d3406b5182df5ee31a0
Tomek Diaków <tomekdiakow@MacBook-Pro-2.local>
2019-04-23 10:52:59 +0200

    Add moveset and anim8 lib

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 function love.load()
-    world = love.physics.newWorld(0, 100)
+    world = love.physics.newWorld(0, 500, false)
+    world:setCallbacks(beginContact, endContact, preSolve, postSolve)
     sprites = {}
     sprites.coin_sheet = love.graphics.newImage('sprites/coin_sheet.png')
     sprites.player_jump = love.graphics.newImage('sprites/player_jump.png')
@@ -8,6 +9,7 @@ function love.load()
     platforms = {}
 
     require('player')
+    anim8 = require('anim8/anim8')
 
     spawnPlatform(50, 400, 300, 30)
 end
@@ -15,16 +17,35 @@ end
 
 function love.update(dt)
     world:update(dt)
+    playerUpdate(dt)
 end
 
 
 function love.draw()
-    love.graphics.draw(sprites.player_stand, player.body:getX(), player.body:getY(), nil, nil, nil, sprites.player_stand:getWidth()/2, sprites.player_stand:getHeight()/2)
+    love.graphics.draw(player.sprite, player.body:getX(), player.body:getY(), nil, player.direction, 1, sprites.player_stand:getWidth()/2, sprites.player_stand:getHeight()/2)
 
     for i, platform in ipairs(platforms) do
         love.graphics.rectangle("fill", platform.body:getX(), platform.body:getY(), platform.width, platform.height)
     end
 end
+
+
+function love.keypressed(key, scancode, isrepeat)
+    if key == "up" and player.grounded then
+        player.body:applyLinearImpulse(0, -2500)
+    end
+end
+
+
+function beginContact(a, b, coll)
+    player.grounded = true
+end
+
+
+function endContact(a, b, coll)
+    player.grounded = false
+end
+
 
 function spawnPlatform(x, y, width, height)
     local platform = {}
